bed107db05a11949681de305188548b05adae64e
Orhan <ya.eskimo-callboy@yandex.by>
2022-07-25 10:54:30 +0300

    fix: cors

--- server.js
+++ server.js
@@ -19,12 +19,8 @@ mysqlConnection.connect((error) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://qaq-app-dev.asist-lab.com")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next()
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    next();
 })
 
 const route = require('./settings/routes');
