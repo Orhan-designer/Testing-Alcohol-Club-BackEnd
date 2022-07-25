c60109b990be04384163985874601facb8cbc3d2
Orhan <ya.eskimo-callboy@yandex.by>
2022-07-25 12:18:12 +0300

    fix: routes

--- settings/routes.js
+++ settings/routes.js
@@ -9,10 +9,10 @@ module.exports = (app) => {
     app.route('/api/users').get(auth, usersListController.getAllUsers);
 
     app.route('/').get((req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-        res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+        res.header('Access-Control-Allow-Credentials', true); // If needed
 
         res.send('cors problem fixed:)')
     })
