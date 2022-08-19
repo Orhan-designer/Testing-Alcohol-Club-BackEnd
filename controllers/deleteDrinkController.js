8c62c7ea2103ed811d7d1d36194d97f5184f83d2
Orhan <ya.eskimo-callboy@yandex.by>
2022-08-19 18:44:12 +0300

    no changes

--- controllers/deleteDrinkController.js
+++ controllers/deleteDrinkController.js
@@ -1,34 +1,48 @@
-const MongoClient = require('mongodb').MongoClient;
-const mongoUrl = 'mongodb+srv://Orhan:Mamedov03Danskih09@cluster0.md34d.mongodb.net/?retryWrites=true&w=majority';
+const MongoClient = require("mongodb").MongoClient;
+const mongoUrl =
+  "mongodb+srv://Orhan:Mamedov03Danskih09@cluster0.md34d.mongodb.net/?retryWrites=true&w=majority";
 const mongoClient = new MongoClient(mongoUrl);
-const ObjectId = require('mongodb').ObjectId;
-const mysqlDb = require('./../settings/mysqlDb');
+const ObjectId = require("mongodb").ObjectId;
+const mysqlDb = require("./../settings/mysqlDb");
 
 exports.deleteDrink = (req, res) => {
-    try {
-        const mongoId = req.query.mongoId;
+  console.log(req.query);
+  try {
+    const mongoId = req.query.mongoId;
 
-        mongoClient.connect((error, client) => {
-            const db = client.db('tastingclub');
+    mongoClient.connect((error, client) => {
+      const db = client.db("tastingclub");
 
-            db.collection('drinks').deleteOne({ '_id': ObjectId(mongoId) }, (err, result) => {
-                if (err) {
-                    return res.status(400).json({ error: 'An error occurred while deleting the drink' });
-                } else {
-                    const userId = req.query.userId;
-                    const deleteFromTable = "DELETE FROM drinksRating WHERE userId = '" + userId + "' AND mongoId = '" + mongoId + "'";
+      db.collection("drinks").deleteOne(
+        { _id: ObjectId(mongoId) },
+        (err, result) => {
+          if (err) {
+            return res
+              .status(400)
+              .json({ error: "An error occurred while deleting the drink" });
+          } else {
+            const userId = req.query.userId;
+            const deleteFromTable =
+              "DELETE FROM drinksRating WHERE userId = '" +
+              userId +
+              "' AND mongoId = '" +
+              mongoId +
+              "'";
 
-                    mysqlDb.query(deleteFromTable, (error, deleteTableResult) => {
-                        if (error) {
-                            return res.status(400).json({ error: "This drink not exist" });
-                        } else {
-                            return res.status(200).json({ message: 'Drink deleted', result: result });
-                        }
-                    });
-                }
+            mysqlDb.query(deleteFromTable, (error, deleteTableResult) => {
+              if (error) {
+                return res.status(400).json({ error: "This drink not exist" });
+              } else {
+                return res
+                  .status(200)
+                  .json({ message: "Drink deleted", result: result });
+              }
             });
-        });
-    } catch (error) {
-        return res.status(400).json({ error: error });
-    }
-}
+          }
+        }
+      );
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error });
+  }
+};
